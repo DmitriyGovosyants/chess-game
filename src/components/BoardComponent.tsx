750c7335858cbfd58c005dfb2f69ec64443f90ac
DmitriyGovosyants <Demiangvs@gmail.com>
2023-09-10 20:07:14 +0300

    add board and figures

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,27 @@
+import React, { FC, Fragment } from 'react';
+import { Board } from '../models/Board';
+import CellComponent from './CellComponent';
+
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+  return (
+    <div className='board'>
+      {board.cells.map((row, index) => 
+        <Fragment key={index}>
+          {row.map(cell => 
+            <CellComponent
+              cell={cell}
+              key={cell.id}
+            /> 
+          )}
+        </Fragment>
+      )}
+    </div>
+  );
+};
+
+export default BoardComponent;
